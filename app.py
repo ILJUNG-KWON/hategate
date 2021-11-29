61b1bf8a3e6d7eeb84c8f981e373c37eb17d68ed
ilju kwon <11jung22kk22@gmail.com>
2021-11-29 06:26:28 +0000

    message

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 #	https://goldengate1.run.goorm.io/home
 from flask import Flask, render_template, request, url_for
 from PIL import Image
-import test
-import fword
+import model
+import slangword
 import purify
 
 app = Flask(__name__)
@@ -14,14 +14,18 @@ def home():
 @app.route('/result', methods=['POST', 'GET'])
 def result_message():
   text = request.form['user_mes']
-  g_m = test.mespredict(text)
-  s_m = fword.slang(text)
+  g_m = model.mespredict(text)
+  s_m = slangword.slang(text)
   p_m = purify.purifier(text)
-  result1 = "{0} {1}".format(s_m, g_m)
-  result2 = p_m
+  # result1 = "{0} {1}".format(s_m, g_m)
+  result1 = s_m
+  result2 = g_m
+  result3 = p_m
+    
   cnt=len(g_m)
-  return render_template('index2.html', result1 = result1, result2=result2, cnt=cnt)
+  return render_template('index2.html', result1 = result1, result2=result2, result3=result3, cnt=cnt)
 
 
 if __name__ == "__main__":
+    #app.run(debug=True)
     app.run(host = '0.0.0.0')
--- app2.py
+++ app2.py
@@ -7,17 +7,14 @@ app2 = Flask(__name__)
     
 @app2.route('/home')
 def index():
-    return render_template('index.html')
-
-
-@app2.route('/popup')
-def popup():
     return render_template('popup.html')
+
 
 
 @app2.route('/result', methods=['POST', 'GET'])
 def result_message():
-    return render_template('index.html', result = result)
+    text = request.form['user_mes']
+    return render_template('popup.html', result1 = result1, result2=result2, cnt=cnt)
 
 if __name__ == "__main__":
     app2.run(host = '0.0.0.0')
